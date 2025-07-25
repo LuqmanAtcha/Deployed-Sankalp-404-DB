a7de4c618ad9d24f088627643f1f92ccfde3ad72
Luqman <123114107+LuqmanAtcha@users.noreply.github.com>
2025-07-25 09:51:47 -0400

    Update auth.ts

--- frontend/src/services/auth.ts
+++ frontend/src/services/auth.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 import { LoginCredentials, LoginResponse } from "../types/auth.js";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api/auth",
+  baseURL: "https://deployed-sankalp-404-db.onrender.com/api/auth",
   headers: {
     "Content-Type": "application/json",
   },
